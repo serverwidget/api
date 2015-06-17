1db1b2c6f5408a1758035124dd1fb5cf694f8037
SERVERWIDGET <admin@serverwidget.com>
2015-06-18 02:00:36 +0300

    Update expample.php

--- expample.php
+++ expample.php
@@ -246,7 +246,5 @@ span.color-green {
 
     <div class="footer">jtiq &copy; SERVERWIDGET, 2015</div>
   </div>
-
-<!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter30890996 = new Ya.Metrika({id:30890996, clickmap:true, trackLinks:true, accurateTrackBounce:true, ut:"noindex"}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/30890996?ut=noindex" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
 </body>
 </html>
